7b4ee69858dfc356f94d160273d6b52c35ea0532
Jonathan Mares <jm2242@cornell.edu>
2017-12-06 11:27:49 -0500

    first commit for apollo-graphql graphcool tutorial

---
+++ hackernews-react-apollo/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+
+// apollo related packages
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const httpLink = new HttpLink({ uri: '__SIMPLE_API_ENDPOINT__' })
+
+// instantiate the client
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  , document.getElementById('root')
+);
+registerServiceWorker();
